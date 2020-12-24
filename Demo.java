1bdc4bfc407af3a3d008219d1493ed236917c7a8
madhusudhan003 <madhusudhan003@gmail.com>
2020-12-24 15:41:34 +0000

    Main brnach code

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
-Hello I am from main brnach code
-Class Demo
-{
-	Public static void main(){
-	System.out.println(" I am from Main branch");
+I am from Main
+Class Demo{
+	Public static void main(String[] args){
+		System.out.println("I am from Main");
+	}
 }
+
